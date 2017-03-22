3d263ca88f20bfcc57f953f8b5d05fff86d8e5ba
David Remington <david.ian.remington@gmail.com>
2017-03-22 14:23:37 -0700

    add and configure router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,23 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
-
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 
-render(<App/>, document.querySelector('#main'));
+const Main = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match exactly pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Main/>, document.querySelector('#main'));
